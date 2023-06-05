728dd60c8735993cecf4b98669c0db574eac34ba
johnh01 <jqcodel@gmail.com>
2023-06-05 14:17:24 +0900

    v1.0.1

--- Formula/devq.rb
+++ Formula/devq.rb
@@ -1,7 +1,7 @@
 class Devq < Formula
   desc "developer cli tool "
   homepage "https://your.program/homepage"
-  url "https://github.com/rogJohn01/homebrew-tap/releases/tag/v1.0.0/devQ6"
+  url "https://github.com/rogJohn01/homebrew-tap/releases/tag/v1.0.1/devQ6"
   sha256 "cc2217ef0f6552fffe69f9315690f019904f26976705ca6704bd37475f5d53c0"
   def install
     bin.install "devQ6"

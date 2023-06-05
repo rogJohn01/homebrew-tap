65d9483ad203196f15d3f45fbc4af227fdb61b12
johnh01 <jqcodel@gmail.com>
2023-06-05 14:13:52 +0900

    modify sh256

--- Formula/devq.rb
+++ Formula/devq.rb
@@ -2,8 +2,7 @@ class Devq < Formula
   desc "developer cli tool "
   homepage "https://your.program/homepage"
   url "https://github.com/rogJohn01/homebrew-tap/releases/tag/v1.0.0/devQ6"
-  sha256 "8a676e4be9e021fd2cf83b2e910b95dfada2357b38d786fc6f9ca5098e167529"
-
+  sha256 "cc2217ef0f6552fffe69f9315690f019904f26976705ca6704bd37475f5d53c0"
   def install
     bin.install "devQ6"
   end
